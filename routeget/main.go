43c56549b555acc6aa78388b61f12e8b0d1cd182
Carlos Hernandez <carlos@hrndz.ca>
2025-02-07 09:01:27 -0700

    feat: confirm reply

--- routeget/main.go
+++ routeget/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"syscall"
 
+	"github.com/hurricanehrndz/examples/rtmprint"
 	"golang.org/x/net/route"
 	"golang.org/x/sys/unix"
 )
@@ -14,16 +14,16 @@ func main() {
 	// Open the AF_ROUTE socket directly using unix.Socket
 	fd, err := unix.Socket(unix.AF_ROUTE, unix.SOCK_RAW, unix.AF_UNSPEC)
 	if err != nil {
-		fmt.Println("Error opening AF_ROUTE socket:", err)
 		os.Exit(1)
 	}
 	defer unix.Close(fd)
 
+	pid := os.Getpid()
 	// Create a RouteMessage with RTM_GET type
 	rtm := &route.RouteMessage{
 		Version: syscall.RTM_VERSION,
 		Type:    unix.RTM_GET,
-		ID:      uintptr(os.Getpid()),
+		ID:      uintptr(pid),
 		Seq:     0,
 		Addrs: []route.Addr{
 			&route.Inet4Addr{IP: [4]byte{127, 0, 0, 0}},
@@ -44,28 +44,30 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Read the response from the routing socket
+	// wait for reply
 	var buf [2 << 10]byte
-	n, err := unix.Read(fd, buf[:])
-	if err != nil {
-		fmt.Println("Error reading from AF_ROUTE socket:", err)
-		os.Exit(1)
+	for {
+		// Read the response from the routing socket
+		n, err := unix.Read(fd, buf[:])
+		if err != nil {
+			fmt.Println("Error reading from AF_ROUTE socket:", err)
+			os.Exit(1)
+		}
+		// Parse the response messages
+		msgs, err := route.ParseRIB(route.RIBTypeRoute, buf[:n])
+		if err != nil {
+			fmt.Println("Error parsing RIB:", err)
+			os.Exit(1)
+		}
+		if len(msgs) == 0 {
+			continue
+		}
+		routeMsg, ok := msgs[0].(*route.RouteMessage)
+		// confirm it is a reply to our query
+		if !ok || (routeMsg.ID != uintptr(pid) && routeMsg.Seq != 0 && routeMsg.Type != unix.RTM_GET) {
+			continue
+		}
+		rtmprint.LogRouteMessages(msgs)
+		break
 	}
-	log.Printf("read %d bytes (% 02x)\n", n, buf[:n])
-
-	// Parse the response messages
-	msgs, err := route.ParseRIB(route.RIBTypeRoute, buf[:n])
-	if err != nil {
-		fmt.Println("Error parsing RIB:", err)
-		os.Exit(1)
-	}
-	routeMsg, ok := msgs[0].(*route.RouteMessage)
-	if !ok {
-		os.Exit(1)
-	}
-	netmask, ok := routeMsg.Addrs[2].(*route.Inet4Addr)
-	if !ok {
-		os.Exit(1)
-	}
-	fmt.Printf("netmask: %v\n", netmask.IP)
 }
